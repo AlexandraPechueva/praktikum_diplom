be2c9dc0f855c91cdea85be40169221b58c585dc
Alexandra <AlexandraSchetchikova@yandex.ru>
2020-03-20 08:51:10 +0300

    Added About page with slider and fix main page

--- src/about/index.js
+++ src/about/index.js
@@ -1,1 +1,26 @@
 import '../pages/about.css';
+import Swiper from 'swiper';
+
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 3.5,
+    centeredSlides: true,
+    spaceBetween: 16,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    fadeEffect: {
+        crossFade: true
+    },
+});
