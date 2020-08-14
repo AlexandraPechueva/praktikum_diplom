7944c9afd08e654de470e14b500848a07ad59c29
Alexandra <AlexandraSchetchikova@yandex.ru>
2020-08-14 16:17:28 +0300

    Change Api address

--- src/js/constatnts/news-api-access-data.js
+++ src/js/constatnts/news-api-access-data.js
@@ -1,6 +1,7 @@
 const NEWS_API_ACCESS_DATA = {
     // address: 'https://newsapi.org/v2/everything?',
-    address: 'https://praktikum.tk/news/v2/everything?',
+    // address: 'https://praktikum.tk/news/v2/everything?',
+    address: 'https://nomoreparties.co/news/v2/everything?',
     token: '24dc55a83df54dfeb22d95136901e62f'
 };
 
